e924dca5e7050c7ac667de5048131ee17d615862
greenblitz <greenblitz@gmail.com>
2025-07-30 15:50:38 +0300

    q1 really

--- main/src/Exceptions.java
+++ main/src/Exceptions.java
@@ -1,8 +1,11 @@
 public class Exceptions {
 
-	public static void q1() {
+	public static void q1() throws Exception{
 		Exception e = new RuntimeException("runtime");
 		Exception boundsE = new IndexOutOfBoundsException("out of bounds");
+
+		boundsE.printStackTrace();
+		throw e;
 	}
 
 }
