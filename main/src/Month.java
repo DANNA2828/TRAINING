bd5593b3f8c81c89f440e09bf777a81c6e7d7f69
greenblitz <greenblitz@gmail.com>
2025-07-30 17:26:52 +0300

    q2 +3 +4

--- main/src/Month.java
+++ main/src/Month.java
@@ -1,16 +1,34 @@
 public enum Month {
 
-	JAN,
-	FEB,
-	MAR,
-	APR,
-	MAY,
-	JUN,
-	JUL,
-	AUG,
-	SEP,
-	OCT,
-	NOV,
-	DEC
+	JAN(30),
+	FEB(30),
+	MAR(30),
+	APR(30),
+	MAY(30),
+	JUN(30),
+	JUL(30),
+	AUG(30),
+	SEP(30),
+	OCT(30),
+	NOV(30),
+	DEC(30);
+
+	private int days;
+
+	Month(int days) {
+		this.days = days;
+	}
+
+	public int getOrdNum() {
+		return ordinal();
+	}
+
+	public static int func(Month month, int day) {
+		int count = day;
+		for (int i = 0; i < month.getOrdNum(); i++) {
+			count += Month.values()[i].days;
+		}
+		return count;
+	}
 
 }
